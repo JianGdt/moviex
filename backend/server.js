c59fea5648c392f824d52631b1c767429a6e2b45
zaijan <jgodito27@gmail.com>
2025-01-07 22:47:33 +0800

    debugging server and config

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movies.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import searchRoutes from "./routes/search.route.js";
 import { ENV_VARS } from "./config/envConfig.js";
-import { connectDB } from "./config/database.js";
+
+import {connectDB} from "./config/database.js";
 import { protectedRoutes } from "./middleware/protectedRoutes.js";
 import cors from "cors";
 
 const app = express();
 
 const PORT = ENV_VARS.PORT;
-const __dirname = path.resolve();
 
 app.use(cors());
 
@@ -31,7 +30,8 @@ app.use("/api/v1/movie", protectedRoutes, movieRoutes);
 app.use("/api/v1/tv", protectedRoutes, tvRoutes);
 app.use("/api/v1/search", protectedRoutes, searchRoutes);
 
-connectDB();
+console.log(process.env.MONGO_URI);
 app.listen(PORT, () => {
 	console.log("Server started at http://localhost:" + PORT);
+  connectDB()
 });
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "https://moviex-apis.onrender.com",
+				target: "http://localhost:8080",
 			},
 		},
 	},
