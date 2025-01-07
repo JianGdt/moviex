beaeab320cd689bc1359d4230c59aaf11a53f2e2
zaijan <jgodito27@gmail.com>
2025-01-08 00:25:20 +0800

    update backend

--- backend/server.js
+++ backend/server.js
@@ -5,33 +5,48 @@ import movieRoutes from "./routes/movies.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import searchRoutes from "./routes/search.route.js";
 import { ENV_VARS } from "./config/envConfig.js";
-
 import {connectDB} from "./config/database.js";
 import { protectedRoutes } from "./middleware/protectedRoutes.js";
 import cors from "cors";
+import dotenv from "dotenv"; 
+
 
 const app = express();
-
 const PORT = ENV_VARS.PORT;
-
-app.use(cors());
-
 app.use(express.json());
+app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.use(cors({
-	origin: ["http://localhost:5173", "https://moviex-apis.onrender.com/"],
-    credentials: true,
-	methods: ["POST", "PUT", "DELETE", "OPTIONS", "GET"]
+	origin: ENV_VARS.CLIENT_URL,
+  credentials: true,
+	method: ["POST", "PUT", "DELETE", "GET"]
 }));
+
+
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectedRoutes, movieRoutes);
 app.use("/api/v1/tv", protectedRoutes, tvRoutes);
 app.use("/api/v1/search", protectedRoutes, searchRoutes);
 
-console.log(process.env.MONGO_URI);
+if(ENV_VARS.NODE_ENV !== "production") {
+  dotenv.config({
+    path: "./.env"
+  })
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+console.log(ENV_VARS.MONGO_URI);
+
+connectDB()
+// db connection
+
 app.listen(PORT, () => {
-	console.log("Server started at http://localhost:" + PORT);
-  connectDB()
+  console.log(`Server running on port ${PORT}`);
+}).on('error', (err) => {
+  console.error(`Failed to start server: ${err.message}`);
 });
