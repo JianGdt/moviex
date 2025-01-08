652433900b80d54ffa29f5b8e80714acf5aee33d
zaijan <jgodito27@gmail.com>
2025-01-09 01:18:24 +0800

    deployment render.com

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,23 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movies.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import searchRoutes from "./routes/search.route.js";
 import { connectDB } from "./config/database.js";
 import { protectedRoutes } from "./middleware/protectedRoutes.js";
+import { ENV_VARS } from "./config/envConfig.js";
 
 // Load environment variables
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json());
@@ -28,9 +33,12 @@ app.use(cors(
   }
 ));
 
-app.get("/", (req, res) => {
-res.json("Hello");
-}) 
+if (ENV_VARS.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 // Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectedRoutes, movieRoutes);
@@ -38,9 +46,9 @@ app.use("/api/v1/tv", protectedRoutes, tvRoutes);
 app.use("/api/v1/search", protectedRoutes, searchRoutes);
 
 // Database connection
-connectDB();
 
 // Start server
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }).on("error", (err) => {
